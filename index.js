6dbe46e8783113bb731078e7a07484c076df2dd3
Nishad Aherrao <nishad.aherrao10@gmail.com>
2019-12-05 01:41:56 -0600

    Added a basic command to get info from Bittrex api and format it

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv')
 const TelegramBot = require('node-telegram-bot-api')
+const axios = require('axios')
+const ramda = require('ramda')
 
 dotenv.config()
 
@@ -10,6 +12,29 @@ bot.onText(/\/ping/, msg => {
   bot.sendMessage(msg.chat.id, 'pong')
 })
 
-bot.onText(/\//, msg => {
+bot.onText(/\/repo/, msg => {
   bot.sendMessage(msg.chat.id, 'https://github.com/nishad10/telegramBot')
+})
+
+bot.onText(/\/bittrex/, msg => {
+  axios
+    .get('https://api.bittrex.com/api/v1.1/public/getmarketsummaries')
+    .then(res => res.data)
+    .then(data => data.result)
+    .then(result => {
+      const id = ramda.findIndex(ramda.propEq('MarketName', 'BTC-RADS'))(result)
+      const date = new Date(result[id].Created)
+      bot.sendMessage(
+        msg.chat.id,
+        `${decodeURI('%60BITTREX%60')}
+        \n*High*: ${result[id].High}
+        \n*Low*: ${result[id].Low}
+        \n*Volume*: ${result[id].Volume}
+        \n*Open Buy Orders*: ${result[id].OpenBuyOrders}
+        \n*Open Sell Orders*: ${result[id].OpenSellOrders}
+        \n*Created*: ${date.toDateString()}`,
+        { parse_mode: 'Markdown' }
+      )
+    })
+    .catch(e => bot.sendMessage(msg.chat.id, 'Something went wrong!'))
 })
